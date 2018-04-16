47061e64c4ece6148e509031530182611f666d1a
Vasiliy Konovalov <vasko.main@gmail.com>
2018-04-16 23:45:43 +0300

    Добавление новых тасков

--- javascripts/app.js
+++ javascripts/app.js
@@ -8,6 +8,13 @@ var main = function () {
         "Обновить несколько новых задач",
         "Купить продукты"
     ];
+    function AddTaskFromInputBox() {
+        if ($(".task-input input").val() !== "") {
+            console.log($(".task-input").val());
+            toDos.push($(".task-input").val());
+            $(".tabs a:first-child span").trigger("click");
+        };
+    };
     $(".tabs a span").toArray().forEach(function (element) {
         // создаем обработку щелчков для этого элемента
         $(element).on("click", function () {
@@ -20,7 +27,7 @@ var main = function () {
             $("main .content").empty();
             if ($element.parent().is(":nth-child(1)")) {
                 var $content = $("<ul>");
-                for (var i = toDos.length; i >= 0;i--){
+                for (var i = toDos.length; i >= 0; i--) {
                     $content.append($("<li>").text(toDos[i]));
                 };
                 $("main .content").append($content);
@@ -31,7 +38,17 @@ var main = function () {
                 });
                 $("main .content").append($content);
             } else if ($element.parent().is(":nth-child(3)")) {
-                console.log("Щелчок на третьей вкладке!");
+                $("main .content").append($("<ul>").text("Введите новую задачу:"));
+                $("main .content").append($("<input>").addClass("task-input").on("keypress",
+                    function (event) {
+                        if (event.keyCode === 13) {
+                            AddTaskFromInputBox();
+                        };
+                    }));
+                $("main .content").append($("<button>").text("+").on("click",
+                    function (event) {
+                        AddTaskFromInputBox();
+                    }));
             }
             return false;
         });
