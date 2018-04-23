43745cddd34f094b32c5f8ced37ebf64a54f6190
Vasiliy Konovalov <vasko.main@gmail.com>
2018-04-23 23:01:39 +0300

    mongoose added

--- server.js
+++ server.js
@@ -1,44 +1,36 @@
 var express = require("express"),
     http = require("http"),
-    app = express(),
-    toDos = [{
-            "description": "Купить продукты",
-            "tags": ["шопинг", "рутина"]
-        },
-        {
-            "description": "Сделать несколько новых задач",
-            "tags": ["писательство", "работа"]
-        },
-        {
-            "description": "Подготовиться к лекции в понедельник",
-            "tags": ["работа", "преподавание"]
-        },
-        {
-            "description": "Ответить на электронные письма",
-            "tags": ["работа"]
-        },
-        {
-            "description": "Вывести Грейси на прогулку в парк",
-            "tags": ["рутина", "питомцы"]
-        },
-        {
-            "description": "Закончить писать книгу",
-            "tags": ["писательство", "работа"]
-        }
-    ];
+    mongoose = require("mongoose"),
+    app = express();
 app.use(express.static(__dirname + "/client"));
-http.createServer(app).listen(3000);
-// этот маршрут замещает наш файл
-// todos.json в примере из главы 5
-app.get("/todos.json", function (req, res) {
-    res.json(toDos);
-});
 app.use(express.urlencoded());
+mongoose.connect('mongodb://localhost/amazeriffic');
+var ToDoSchema = mongoose.Schema({
+    description: String,
+    tags: [String]
+})
+var ToDo = mongoose.model("ToDo", ToDoSchema);
+http.createServer(app).listen(3000);
+app.get("/todos.json", function (req, res) {
+    ToDo.find({}, function (err, toDos) {
+        res.json(toDos);
+    });
+});
 app.post("/todos", function (req, res) {
-    var newToDo = req.body;
-    console.log(newToDo);
-    toDos.push(newToDo);// простой объект отправлен обратно
-    res.json({
-        "message": "Вы размещаетесь на сервере!"
+    console.log(req.body);
+    var newToDo = new ToDo({ "description": req.body.description, "tags": req.body.tags });
+    newToDo.save(function (err, result) {
+        if (err != null) {
+            console.log(err);
+            res.send("ERROR");
+        } else {
+            ToDo.find({}, function (err, result) {
+                if (err != null) {
+                    console.log(err);
+                    res.send("ERROR");
+                }
+                res.json(result);
+            });
+        }
     });
 });
